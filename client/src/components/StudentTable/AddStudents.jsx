36e36177176c311e33054e741834f576219851bd
NutonChakma <nuton121@gmail.com>
2021-11-25 12:18:43 +0600

    add more changes

--- client/src/components/StudentTable/AddStudents.jsx
+++ client/src/components/StudentTable/AddStudents.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import MaterialTable from "material-table";
+import MaterialTable from '@material-table/core';
 import { Grid, Container } from "@material-ui/core";
 
 import { tableIcons } from "../../lib/material-table-config";
